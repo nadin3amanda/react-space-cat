559ecefb6c37a265b14fb8a1e643b0862ad5b13b
nadin3amanda <nadine@soar.earth>
2022-04-16 18:45:05 +0800

    finalized styling: color and particles

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,86 @@ function App() {
         console.log(main);
         await loadFull(tsParticles);
       };
-      const particlesLoaded = (container: any) => {
+      const particlesLoaded = async (container: any) => {
         console.log(container);
       };
 
     return (
         <React.Fragment>
-             <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} />
+                <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            "value": "#6cf3f5, #91cefb, #b7b3ce, #efaed0, #f1b8fc "
+          },
+        },
+        fpsLimit: 300,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "bubble"
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none", //
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1000,
+            },
+            value: 150,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 3 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
             <div className="cat-container">
                 <img src="https://github.com/nadin3amanda/Space-Cat/blob/main/images/spacecat.png?raw=true" alt="cat" className="spacecat"/>
             </div>
             <script src="https://cdn.jsdelivr.net/npm/tsparticles@1/tsparticles.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/tsparticles-preset-stars@1/tsparticles.preset.stars.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/tsparticles-preset-stars@1/tsparticles.preset.stars.bundle.min.js"></script>
         </React.Fragment>
     );
