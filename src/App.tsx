d8dd8ccd1932c9e0e6bcdf6ca1bf42243599175e
nadin3amanda <nadine@soar.earth>
2022-04-16 17:33:31 +0800

    scrambled eggs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React, { useCallback } from "react";
+import React from "react";
 import Particles from "react-tsparticles";
-import type { Engine } from "tsparticles-engine";
+import { tsParticles } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 import './App.css';
-import particlesOptions from "./particles.json";
-import { ISourceOptions } from "tsparticles-engine";
+
 
 function App() {
-    const particlesInit = useCallback((engine: Engine) => {
-        loadFull(engine);
-    }, []);
+    const particlesInit = async (main: any) => {
+        console.log(main);
+        await loadFull(tsParticles);
+      };
+      const particlesLoaded = (container: any) => {
+        console.log(container);
+      };
 
     return (
         <React.Fragment>
-            <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
+             <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} />
             <div className="cat-container">
                 <img src="https://github.com/nadin3amanda/Space-Cat/blob/main/images/spacecat.png?raw=true" alt="cat" className="spacecat"/>
             </div>
