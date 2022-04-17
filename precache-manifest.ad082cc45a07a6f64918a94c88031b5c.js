66509b5650b7a689a348ccc1ea93e3bfe2c51560
nadin3amanda <nadine@soar.earth>
2022-04-17 14:17:05 +0800

    Updates

---
+++ precache-manifest.ad082cc45a07a6f64918a94c88031b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4bf3de20c949261cfc1",
+    "url": "/react-space-cat/static/css/main.ff69bc1e.chunk.css"
+  },
+  {
+    "revision": "c4bf3de20c949261cfc1",
+    "url": "/react-space-cat/static/js/main.c4bf3de2.chunk.js"
+  },
+  {
+    "revision": "0d156a7f0f01f5aff8d4",
+    "url": "/react-space-cat/static/js/1.0d156a7f.chunk.js"
+  },
+  {
+    "revision": "90b05800721a07db57af",
+    "url": "/react-space-cat/static/js/2.90b05800.chunk.js"
+  },
+  {
+    "revision": "a381d12a13a93bfb56da",
+    "url": "/react-space-cat/static/js/runtime~main.a381d12a.js"
+  },
+  {
+    "revision": "bc436621530e9a84eb755c3ef013304b",
+    "url": "/react-space-cat/index.html"
+  }
+];
